0c492196259e4176dfaf3ddbe1cd4738df1112f1
Ofreshy <offer.sharabi@booking.com>
2023-10-30 10:19:44 +0200

    Adding tests

--- fbaptiste/bank_accounts/models.py
+++ fbaptiste/bank_accounts/models.py
@@ -7,19 +7,26 @@ import uuid
 
 def generate_id():
     """
-
+    a shorter version of the UUID4
     """
     return str(uuid.uuid4())[0:8]
 
 
 @dataclass
 class Person:
+    """
+    Couple of details on the person
+    """
     id_: str
     first_name: str
     last_name: str
+    # middle name is optional; Use empty space if none exist
     middle_name: str
 
     def __post_init__(self):
+        """
+        Capitalize names
+        """
         self.first_name = self.first_name.lower().capitalize()
         self.last_name = self.last_name.lower().capitalize()
         self.middle_name = self.middle_name.lower().capitalize()
@@ -32,6 +39,9 @@ class Person:
 
 
 class PeopleFactory:
+    """
+    Controls id generation and stores all people by their id
+    """
     def __init__(self):
         self._people = dict()
 
@@ -47,11 +57,21 @@ class PeopleFactory:
 
 
 class TransactionCommand(ABC):
+    """
+    Captures all Transaction commands (Deposit, Withdraw, Interest etc.)
+    Where each transaction impelment what it needs (no shared behaviour as such)
+    """
     pass
 
 
 @dataclass
 class Deposit(TransactionCommand):
+    """
+    Use to deposit money
+
+    Why do we need it and not just int?
+    Can Use OO concepts and validations are handy
+    """
     value: float
 
     def __post_init__(self):
@@ -63,6 +83,10 @@ class Deposit(TransactionCommand):
 
 @dataclass
 class Interest(TransactionCommand):
+    """
+    Use to apply interest to account;
+    See that the value is taken to be the interest rate, so 0.1 is 10% and means adding 10% to value.
+    """
     value: float
 
     def __post_init__(self):
@@ -74,9 +98,16 @@ class Interest(TransactionCommand):
 
 @dataclass
 class Withdraw(TransactionCommand):
+    """
+    Withdraw money command.
+
+    """
     value: float
 
     def __post_init__(self):
+        """
+        Some arbitrary upper bound on the value just to show the validation
+        """
         if self.value < 0 or self.value > 10000:
             raise ValueError(
                 f"Withdraw value must be between 0-10000 inc. and was {self.value}"
@@ -88,20 +119,33 @@ class BankAccount:
     id_: str
     person: Person
     time_zone: datetime.tzinfo
+
+    # Balance is computed
     _balance: float = 0
+    # overdraft not to be set directly
     _overdraft: float = 0
 
     def deposit(self, deposit: Deposit):
+        """
+        Since we use deposit type we know that it has a positive value
+        """
         self._balance += deposit.value
 
     def interest(self, interest: Interest):
-        if not self.is_overdrawn():
+        """
+        only apply interest if the account is in credit
+        """
+        if self.is_in_credit():
             self._balance *= (1 + interest.value)
 
     def withdraw(self, withdraw: Withdraw):
+        """
+        Withdraw is valid if the bank account has not exceed overdraft
+        """
         new_balance = self._balance - withdraw.value
-        if new_balance < self._overdraft * -1:
-            raise ValueError(" cannot exceed overdraft")
+        if new_balance < self.overdraft * -1:
+            raise ValueError(
+                f"new balance {new_balance} cannot exceed overdraft of {self.overdraft}")
         self._balance = new_balance
 
     @property
@@ -114,13 +158,26 @@ class BankAccount:
 
     @overdraft.setter
     def overdraft(self, value):
+        # We store the positive value of overdraft
         self._overdraft = value
 
-    def is_overdrawn(self):
-        return self._balance < 0
+    def is_in_credit(self):
+        return self._balance > 0
+
+    def __repr__(self):
+        return f"BankAccount(" \
+               f"person={self.person}" \
+               f", id={self.id_}" \
+               f", balance={self.balance}" \
+               f", overdraft={self.overdraft}" \
+               f", preferred_time_zone={self.time_zone.tzname('')}" \
+               f")"
 
 
 class BankAccountFactory:
+    """
+    Use for opening bank account and generating ids
+    """
 
     def __init__(self):
         self._accounts = dict()
@@ -184,6 +241,10 @@ class TransactionResult:
 
 @dataclass
 class Transaction:
+    """
+    Breaking the transaction info into classes allow us to not mutate them
+    and have a clear responsibility for each method / step, in line with server/client
+    """
     id_: str
     command: TransactionCommand
     details: TransactionDetails
@@ -195,6 +256,17 @@ class TransactionFactory:
         self._transactions = dict()
 
     def _apply(self, bank_account: BankAccount, command, command_func):
+        """
+        Templated method for all commands;
+
+        Generate details
+        Apply the command
+        Get result and update status
+        Store the transaction before returning it
+
+        Note that the caller is not aware for this method,
+        they should use the method (i.e. levers) that we create
+        """
         details = TransactionDetails.from_bank_account(bank_account)
         try:
             command_func(command)
--- fbaptiste/bank_accounts/test_accounts.py
+++ fbaptiste/bank_accounts/test_accounts.py
@@ -1,25 +1,97 @@
-
+import unittest
 from fbaptiste.bank_accounts import models
 
 
-people_factory = models.PeopleFactory()
-bank_account_factory = models.BankAccountFactory()
-transaction_factory = models.TransactionFactory()
+class TestPerson(unittest.TestCase):
+    def test_middle_name_when_not_empty(self):
+        p = models.Person(id_="", first_name="Andrew", middle_name="Charlie", last_name="Hall")
+        expected_name = "Andrew Charlie Hall"
 
-o_sharabi = people_factory.new(first_name="offer", last_name="sharabi")
-ba = bank_account_factory.open(person=o_sharabi, overdraft=100, time_zone=models.ist)
+        self.assertEqual(expected_name, p.full_name())
 
-print(ba)
-print()
+    def test_middle_name_when_empty(self):
+        p = models.Person(id_="", first_name="Andrew", last_name="Hall", middle_name="")
+        expected_name = "Andrew Hall"
 
-t = transaction_factory.deposit(ba, 100)
-print(t)
-print()
-t = transaction_factory.apply_interest(ba, 0.5)
-print(t)
-print()
-t = transaction_factory.withdraw(ba, 101)
-print(t)
-print()
-t = transaction_factory.withdraw(ba, 200)
-print(t)
+        self.assertEqual(expected_name, p.full_name())
+
+    def test_capitalizing(self):
+        p = models.Person(id_="", first_name="andrew", middle_name="charlie", last_name="hall")
+        expected_name = "Andrew Charlie Hall"
+
+        self.assertEqual(expected_name, p.full_name())
+
+
+class TestBankAccount(unittest.TestCase):
+
+    def new_bank_account(self, overdraft=0):
+        return models.BankAccount(
+            id_="test",
+            person=models.Person(id_="", first_name="a", middle_name="c", last_name="b"),
+            time_zone=models.ist,
+            _overdraft=overdraft,
+        )
+
+    def test_new_bank_account_is_0_balance(self):
+        b = self.new_bank_account()
+        self.assertEqual(0.0, b.balance)
+
+    def test_new_bank_account_with_overdraft_set(self):
+        b = self.new_bank_account(overdraft=100)
+        self.assertEqual(100.0, b.overdraft)
+
+    def test_bank_account_update_after_deposit(self):
+        b = self.new_bank_account()
+        b.deposit(models.Deposit(value=100))
+        self.assertEqual(100.0, b.balance)
+
+    def test_bank_account_update_after_withdraw(self):
+        b = self.new_bank_account()
+        b.deposit(models.Deposit(value=100))
+        b.withdraw(models.Withdraw(value=50))
+        self.assertEqual(50.0, b.balance)
+
+    def test_bank_account_update_after_withdraw_with_overdraft(self):
+        b = self.new_bank_account(overdraft=50)
+        b.deposit(models.Deposit(value=100))
+        b.withdraw(models.Withdraw(value=120))
+        self.assertEqual(-20.0, b.balance)
+
+    def test_bank_account_update_after_withdraw_without_overdraft(self):
+        b = self.new_bank_account()
+        b.deposit(models.Deposit(value=100))
+        with self.assertRaises(ValueError):
+            b.withdraw(models.Withdraw(value=150))
+
+    def test_bank_account_update_after_withdraw_when_exceeds_overdraft(self):
+        b = self.new_bank_account(overdraft=20)
+        b.deposit(models.Deposit(value=100))
+        with self.assertRaises(ValueError):
+            b.withdraw(models.Withdraw(value=150))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# people_factory = models.PeopleFactory()
+# bank_account_factory = models.BankAccountFactory()
+# transaction_factory = models.TransactionFactory()
+#
+# o_sharabi = people_factory.new(first_name="offer", last_name="sharabi")
+# ba = bank_account_factory.open(person=o_sharabi, overdraft=100, time_zone=models.ist)
+#
+# print(ba)
+# print()
+#
+# t = transaction_factory.deposit(ba, 100)
+# print(t)
+# print()
+# t = transaction_factory.apply_interest(ba, 0.5)
+# print(t)
+# print()
+# t = transaction_factory.withdraw(ba, 101)
+# print(t)
+# print()
+# t = transaction_factory.withdraw(ba, 200)
+# print(t)
